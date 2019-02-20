96a06f398b6a7190fe6a647f4af12dcf6b40ff2b
Joel Amálio <joel.amalio@gmail.com>
2019-02-20 15:58:11 -0300

    Solution of the task "Conventions - Operators Overloading".

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -15,6 +15,8 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+
 enum class TimeInterval {
     DAY,
     WEEK,
