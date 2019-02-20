75ef6c320e7b27ca249cf69a44b0acd7e6947d90
Joel Amálio <joel.amalio@gmail.com>
2019-02-20 12:05:42 -0300

    Solution of the task "Conventions - Comparison".

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,17 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (year == other.year) {
+            if (month == other.month) {
+                return dayOfMonth.compareTo(other.dayOfMonth)
+            }
+            return month.compareTo(other.month)
+        }
+
+        return year.compareTo(other.year)
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
